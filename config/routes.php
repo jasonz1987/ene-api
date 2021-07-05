836d8f6c73894e24f17a2fde3776d0e56b5caf6e
Jason.z <281674669@qq.com>
2021-07-05 16:23:47 +0800

    修改路由~

--- config/routes.php
+++ config/routes.php
@@ -12,13 +12,13 @@ declare(strict_types=1);
 use Hyperf\HttpServer\Router\Router;
 use App\Middleware\AuthMiddleware;
 
-Router::addRoute(['GET', 'HEAD'], '/api/auth/info', 'App\Controller\AuthController@info');
-Router::addRoute(['POST', 'HEAD'], '/api/auth/nonce', 'App\Controller\AuthController@nonce');
-Router::addRoute(['POST', 'HEAD'], '/api/auth/token', 'App\Controller\AuthController@token');
+Router::addRoute(['GET', 'HEAD'], '/api/auth/info', 'App\Controller\Api\AuthController@info');
+Router::addRoute(['POST', 'HEAD'], '/api/auth/nonce', 'App\Controller\Api\AuthController@nonce');
+Router::addRoute(['POST', 'HEAD'], '/api/auth/token', 'App\Controller\Api\AuthController@token');
 
 Router::addGroup('/api',function (){
-    Router::addRoute(['GET', 'HEAD'], '/power/index', 'App\Controller\PowerController@start');
-    Router::addRoute(['POST', 'HEAD'], '/power/profit', 'App\Controller\PowerController@stop');
+    Router::addRoute(['GET', 'HEAD'], '/power/index', 'App\Controller\Api\PowerController@start');
+    Router::addRoute(['POST', 'HEAD'], '/power/profit', 'App\Controller\Api\PowerController@stop');
 
 },
     ['middleware' => [AuthMiddleware::class]]
