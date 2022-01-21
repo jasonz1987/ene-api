5c80c9251859f195398ede4dd46a5d6044fc58a3
Jason.z <281674669@qq.com>
2022-01-21 11:54:50 +0800

    22111111111112121112

--- config/autoload/crontab.php
+++ config/autoload/crontab.php
@@ -14,9 +14,9 @@ return [
         (new Crontab())->setType('command')->setName('CheckProfit')->setRule('* * * * *')->setCallback([
             'command' => 'check:profit',
         ]),
-//        (new Crontab())->setType('command')->setName('CheckDeposit')->setRule('* * * * *')->setCallback([
-//            'command' => 'check:deposit',
-//        ]),
+        (new Crontab())->setType('command')->setName('GetPrice')->setRule('* * * * *')->setCallback([
+            'command' => 'get:price',
+        ]),
 //        (new Crontab())->setType('command')->setName('CheckEvent')->setRule('*/5 * * * *')->setCallback([
 //            'command' => 'check:event',
 //        ]),
